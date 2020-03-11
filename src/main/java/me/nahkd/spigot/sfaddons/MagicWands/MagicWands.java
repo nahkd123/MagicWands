42e3cb9bec11052959cc524ab2505a49ee79425c
nahkd123 <nahkd123@gmail.com>
2020-03-11 15:31:04 +0700

    Register MagicWand item

--- src/main/java/me/nahkd/spigot/sfaddons/MagicWands/MagicWands.java
+++ src/main/java/me/nahkd/spigot/sfaddons/MagicWands/MagicWands.java
@@ -13,51 +13,62 @@ import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import me.nahkd.spigot.sfaddons.MagicWands.sf.MagicWand;
 
 public class MagicWands extends JavaPlugin implements SlimefunAddon {
 	
+	public static MagicWands pluginInstance;
+	
+	public static final String CATEGORY_NAME = "§bMagicWands";
+	public static final Category PLUGIN_CATEGORY = new Category(new NamespacedKey(pluginInstance, "magicwands"), new CustomItem(Material.STICK, CATEGORY_NAME));
+	
+	public static MagicWand MAGIC_WAND;
+	
 	@Override
 	public void onEnable() {
+		pluginInstance = this;
+		
 		// Read something from your config.yml
 		Config cfg = new Config(this);
 		
-		if (cfg.getBoolean("options.auto-update")) {
-			// You could start an Auto-Updater for example
-		}
+//		if (cfg.getBoolean("options.auto-update")) {
+//			// You could start an Auto-Updater for example
+//		}SlimefunItems
 		
 		// Slimefun4 also already comes with a bundled version of bStats
 		// You can use bStats to collect usage data about your plugin
 		// More info: https://bstats.org/getting-started
 		// Set bStatsId to the id of your plugin
-		int bStatsId = -1;
-		new Metrics(this, bStatsId);
+//		int bStatsId = -1;
+//		new Metrics(this, bStatsId);
 		
 		// Create a new Category
 		// This Category will use this ItemStack
-		ItemStack categoryItem = new CustomItem(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
-		
-		// Give your Category a unique id.
-		NamespacedKey categoryId = new NamespacedKey(this, "addon_category");
-		Category category = new Category(categoryId, categoryItem);
-		
-		// Create a new Slimefun ItemStack
-		// This class has many constructors, it is very important that you give each item a unique id.
-		SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
-		
-		// The Recipe is an ItemStack Array with a length of 9.
-		// It represents a Shaped Recipe in a 3x3 crafting grid
-		// The machine in which this recipe is crafted in is specified further down
-		ItemStack[] recipe = {
-			new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD),
-			null, new ItemStack(Material.DIAMOND), null,
-			new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD)
-		};
-		
-		// Now you just have to register the item
-		// RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in which this item is crafted in.
-		// Recipy Types from Slimefun itself will automatically add the recipe to that machine
-		SlimefunItem item = new SlimefunItem(category, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
-		item.register(this);
+//		ItemStack categoryItem = new CustomItem(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
+//		
+//		// Give your Category a unique id.
+//		NamespacedKey categoryId = new NamespacedKey(this, "addon_category");
+//		Category category = new Category(categoryId, categoryItem);
+//		
+//		// Create a new Slimefun ItemStack
+//		// This class has many constructors, it is very important that you give each item a unique id.
+//		SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
+//		
+//		// The Recipe is an ItemStack Array with a length of 9.
+//		// It represents a Shaped Recipe in a 3x3 crafting grid
+//		// The machine in which this recipe is crafted in is specified further down
+//		ItemStack[] recipe = {
+//			new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD),
+//			null, new ItemStack(Material.DIAMOND), null,
+//			new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD)
+//		};
+//		
+//		// Now you just have to register the item
+//		// RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in which this item is crafted in.
+//		// Recipy Types from Slimefun itself will automatically add the recipe to that machine
+//		SlimefunItem item = new SlimefunItem(category, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
+//		item.register(this);
+		MAGIC_WAND = new MagicWand(); MAGIC_WAND.register(this);
 	}
 	
 	@Override
