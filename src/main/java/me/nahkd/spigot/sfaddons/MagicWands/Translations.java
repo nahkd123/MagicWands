bf30f2738821418396496e1bf6d48ffddf67a801
nahkd123 <nahkd123@gmail.com>
2020-03-12 20:45:15 +0700

    Add this thing for translating

---
+++ src/main/java/me/nahkd/spigot/sfaddons/MagicWands/Translations.java
@@ -0,0 +1,7 @@
+package me.nahkd.spigot.sfaddons.MagicWands;
+
+public class Translations {
+	
+	public static final String DEATHMSG_STACKOVERFLOWERROR = "%s exceed java.lang.StackOverflowError";
+	
+}
